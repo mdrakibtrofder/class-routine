d6472de422083d41f4a6e84f0d8d7d48c36cc501
Md. Rakib Trofder <mdrakibtrofder@gmail.com>
2026-02-27 23:18:09 +0600

    feat: Wrap application components with the new RoutineDataProvider.

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,17 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter basename="/class-routine">
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <RoutineDataProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter basename="/class-routine">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </RoutineDataProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
